7d412c75d32ba8e3af795c41599edea74e165e00
Thijs van Dijk <github@inurbanus.nl>
2017-11-12 23:09:09 +0100

    Add fruit. Eating it makes you longer.

--- 003-snake.js
+++ 003-snake.js
@@ -3,11 +3,12 @@ var canvas, ctx;
 var width = 800, height = 480;
 var size = 20;
 
-var gameTick = 0.4;
+var gameTick = 0.2;
 var currentTick = 0.0;
 
 var snake;
 var obstacles;
+var fruit;
 
 var setup = function()
 {
@@ -19,9 +20,10 @@ var setup = function()
 	ctx.fillStyle = 'rgb( 20, 20, 20 )';
 	ctx.fillRect( 0, 0, width, height );
 
+	fruit = [];
+	obstacles = [];
 	snake = new Snake(new Grid(0,0), new Grid(1,0));
 
-	obstacles = [];
 	var x0 = width / (size*2);
 	var y0 = height / (size*2);
 
@@ -53,12 +55,27 @@ var draw = function( deltaT )
 		obstacles[i].show();
 	}
 
+	for ( var i = 0; i < fruit.length; i++ )
+	{
+		fruit[i].show();
+	}
+
 	snake.show();
 };
 
 var update = function()
 {
 	currentTick -= gameTick;
+
+	var prob = 0.001;
+	if ( fruit.length == 0 )
+		prob = 0.4;
+	if ( fruit.length == 1 )
+		prob = 0.1;
+
+	if ( Math.random() < prob )
+		newFruit();
+
 	snake.update();
 };
 
@@ -119,12 +136,25 @@ class Snake
 		}
 		var np = this.Head.add( this.Velocity );
 
+		for ( var i = fruit.length - 1; i >= 0; i-- )
+		{
+			if ( fruit[i].Position.eq(np) )
+			{
+				var tt = this.Tail[this.Tail.length - 1];
+				this.Tail.push(tt);
+				this.Tail.push(tt);
+				this.Tail.push(tt);
+
+				fruit.splice( i, 1 );
+			}
+		}
+
 		for ( var i = 0; i < obstacles.length; i++ )
 		{
 			if ( obstacles[i].Position.eq( np ) )
 				this.Dead = true;
 		}
-		for ( var i = 0; i < this.Tail.length; i++ )
+		for ( var i = 0; i < this.Tail.length - 1; i++ )
 		{
 			if ( this.Tail[i].eq( np ) )
 				this.Dead = true;
@@ -153,7 +183,7 @@ class Snake
 			ctx.fillRect( c[0], c[1], size, size );
 		}
 
-		ctx.fillStyle = "rgb( 216, 150, 85 )"
+		ctx.fillStyle = "rgb( 216, 150, 85 )";
 		if ( this.Dead )
 			ctx.fillStyle = "rgb( 151, 124, 97 )";
 		var c = this.Head.absolute();
@@ -170,11 +200,75 @@ class Obstacle
 
 	show()
 	{
-		ctx.fillStyle = "rgb( 200, 200, 200 )"
+		ctx.fillStyle = "rgb( 200, 200, 200 )";
 		var c = this.Position.absolute();
 		ctx.fillRect( c[0], c[1], size, size );
 	}
 }
+
+class Fruit
+{
+	constructor( pos )
+	{
+		this.Position = pos;
+	}
+
+	show()
+	{
+		ctx.fillStyle = "rgb( 212, 132, 200 )";
+		var c = this.Position.absolute();
+		ctx.beginPath();
+		ctx.arc( c[0] + size*0.5, c[1] + size*0.6, size * 0.34, 0, 2*Math.PI, true );
+		ctx.fill();
+
+		ctx.strokeStyle = "rgb( 0, 180, 14 )";
+		ctx.lineWidth = size * 0.07;
+		ctx.beginPath();
+		ctx.arc( c[0] + size*0.75, c[1] + size*0.45, size * 0.2, 1.1*Math.PI, 1.6*Math.PI, false );
+		ctx.stroke();
+	}
+}
+
+var newFruit = function()
+{
+	var x0 = width / (size*-2);
+	var y0 = height / (size*-2);
+
+	var x1 = ( width / size ) - 1;
+	var y1 = ( height / size ) - 1;
+
+	while ( true )
+	{
+		var x = Math.floor( x0 + Math.random() * x1 );
+		var y = Math.floor( y0 + Math.random() * y1 );
+
+		var np = new Grid(
+			Math.floor( x0 + Math.random() * x1 ),
+			Math.floor( y0 + Math.random() * y1 ) );
+
+		if ( snake.Head.eq( np ) )
+			continue;
+
+		var okay = true;
+
+		for ( var i = 0; i < obstacles.length; i++ )
+		{
+			if ( obstacles[i].Position.eq( np ) )
+				okay = false;
+		}
+		for ( var i = 0; i < snake.Tail.length; i++ )
+		{
+			if ( snake.Tail[i].eq( np ) )
+				okay = false;
+		}
+
+		if ( !okay )
+			continue;
+
+		fruit.push( new Fruit( np ) );
+		return;
+	};
+};
 
 
 window.addEventListener( "keydown", function(event)
