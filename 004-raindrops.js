b4eddcc9da8edbe6db34f97443359e597ce25ee5
Thijs van Dijk <github@inurbanus.nl>
2017-11-13 20:48:49 +0100

    Add splash animation

--- 004-raindrops.js
+++ 004-raindrops.js
@@ -6,7 +6,8 @@ var skyColour = "rgb( 134, 146, 153 )";
 var dropColour = "rgb( 88, 117, 132 )";
 var dropsize = 15;
 
-var speed = height * 10.0;
+var speed = height * 12.0;
+var splashSpeed = 8;
 
 
 var drops;
@@ -61,12 +62,27 @@ class Drop
 		this.scale = 1 / (this.Y + 1);
 
 		this.X = (Math.random() - 0.5) * width / this.scale;
-		this.Z = 16.0 * height;
+
+		this.Z = (12.0 + 4.0*Math.random()) * height;
+		this.splashAnimation = 0.0;
 	}
 
 	update( deltaT )
 	{
-		this.Z -= speed * deltaT;
+		if ( this.Z <= 0 )
+		{
+			this.splashAnimation += deltaT * splashSpeed;
+		}
+		else
+		{
+			this.Z -= speed * deltaT;
+
+			if ( this.Z < 0 )
+			{
+				this.splashAnimation -= (this.Z/speed) * splashSpeed;
+				this.Z = 0.0;
+			}
+		}
 	}
 
 	show()
@@ -79,14 +95,43 @@ class Drop
 		var absX = 0.5*width + this.X * this.scale;
 		var absY = -1.0 * (this.Z-(height*1.1)) * this.scale + height*0.7;
 
-		ctx.moveTo( absX, absY );
-		ctx.lineTo( absX, absY - dropsize * 4 * this.scale );
+		var dss = dropsize * this.scale;
+
+		if ( this.Z <= 0 )
+		{
+			var r0 = 0.0;
+			var r1 = this.splashAnimation;
+
+			if ( this.splashAnimation > 0.5 )
+				r0 = (this.splashAnimation - 0.5) * 2.0;
+
+			ctx.moveTo( absX - 3*r0*dss, absY - 3*r0*dss );
+			ctx.lineTo( absX - 3*r1*dss, absY - 3*r1*dss );
+
+			ctx.moveTo( absX + 3*r0*dss, absY - 3*r0*dss );
+			ctx.lineTo( absX + 3*r1*dss, absY - 3*r1*dss );
+
+			if ( this.splashAnimation < 0.7 )
+			{
+				ctx.moveTo( absX, absY );
+				ctx.lineTo( absX, absY - 4*(1.0-(this.splashAnimation * 1.428))*dss );
+			}
+		}
+		else
+		{
+			ctx.moveTo( absX, absY );
+			ctx.lineTo( absX, absY - 4*dss );
+		}
+
 		ctx.stroke();
 	}
 
 	done()
 	{
-		return (this.Z < 0);
+		if ( this.Z > 0 )
+			return false;
+
+		return ( this.splashAnimation >= 1.0 );
 	}
 }
 
