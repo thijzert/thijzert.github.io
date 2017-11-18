5b5ae1b397cb41165907972499da5e1edbb7922f
Thijs van Dijk <github@inurbanus.nl>
2017-11-18 12:30:15 +0100

    Add pixel art

--- 005-space-invaders.js
+++ 005-space-invaders.js
@@ -4,13 +4,17 @@ var width = 800, height = 480;
 var currentlyPressed;
 
 var enemySpeed = width / 10.0;
-var enemyStages = [ 0.8, 0.35 ];
+var enemyStages = [ 0.8, 0.15 ];
 
 var playerSpeed = width / 3.0;
 var bulletSpeed = height / 1.0;
 
 var waveTime = 30.0
 var waveCounter;
+
+var sprites;
+var spriteSize = 7;
+
 
 var setup = function()
 {
@@ -22,11 +26,51 @@ var setup = function()
 	ctx.fillStyle = 'rgb( 20, 20, 20 )';
 	ctx.fillRect( 0, 0, width, height );
 
+	sprites = {
+		enemyA: new Sprite(
+			"  x     x  ",
+			"   x   x   ",
+			"  xxxxxxx  ",
+			" xx xxx xx ",
+			"xxxxxxxxxxx",
+			"x xxxxxxx x",
+			"x x     x x",
+			"   xx xx   "
+		),
+		enemyB: new Sprite(
+			"   x   x   ",
+			" x  xxx  x ",
+			" x xxxxx x ",
+			" xxx x xxx ",
+			"  xxxxxxx  ",
+			"   x   x   ",
+			"  x     x  "
+		),
+		enemyC: new Sprite(
+			"     x     ",
+			"    xxx    ",
+			"   xxxxx   ",
+			"  xxxxxxx  ",
+			" xx  x  xx ",
+			" xxxxxxxxx ",
+			"   xxxxx   ",
+			"  x  x  x  ",
+			" x  x x  x "
+		),
+		player: new Sprite(
+			"      x      ",
+			"     xxx     ",
+			" xxxxxxxxxxx ",
+			"xxxxxxxxxxxxx",
+			"xxxxxxxxxxxxx"
+		)
+	};
+
 	player = new Player();
 	enemies = [];
 	bullets = [];
 
-	waveCounter = waveTime - 2.5;
+	waveCounter = waveTime - 0.5;
 	currentlyPressed = { h: false, l: false };
 };
 
@@ -41,14 +85,14 @@ var draw = function( deltaT )
 		enemies[i].draw();
 	}
 
-	player.update( deltaT );
-	player.draw();
-
 	for ( var i = bullets.length - 1; i >= 0; i-- )
 	{
 		bullets[i].update( deltaT );
 		bullets[i].draw();
 	}
+
+	player.update( deltaT );
+	player.draw();
 
 	waveCounter += deltaT;
 	if ( waveCounter >= waveTime )
@@ -62,16 +106,28 @@ var draw = function( deltaT )
 var wave = function()
 {
 	var enemyMargin = 20.0;
+	var enemySpacing = spriteSize * 1;
+	var enemyWidth = sprites.enemyA.size()[0];
+
 	var enemiesX = 7;
-	var spacingX = ( width - enemyMargin*2 - 80.0 - (enemyStages[0]*enemySpeed) ) / ( enemiesX - 1 );
+	var spacingX = ( width - enemyMargin*2 - enemyWidth - (enemyStages[0]*enemySpeed) ) / ( enemiesX - 1 );
 
-	for ( var i = 0; i < enemiesX; i++ )
+	var Y = 10;
+
+	for ( var j = 0; j < 5; j++ )
 	{
-		var x = enemyMargin + 40.0 + i*spacingX;
+		var t = 0;
+		if ( j == 4 )      t = 2;
+		else if ( j >= 2 ) t = 1;
 
-		for ( var j = 0; j < 4; j++ )
+		var e = new Enemy( x, -1*Y, t );
+		Y += e.Sprite.size()[1] + enemySpacing;
+
+		for ( var i = 0; i < enemiesX; i++ )
 		{
-			enemies.push( new Enemy( x, (j+1) * -60 ) );
+			var x = enemyMargin + (0.5*enemyWidth) + i*spacingX;
+
+			enemies.push( new Enemy( x, -1*Y, t ) );
 		}
 	}
 };
@@ -84,15 +140,15 @@ class Player
 	constructor()
 	{
 		this.X = width / 2;
-		this.Y = height - 40;
+		this.Y = height - sprites.player.size()[1];
+		this.xoff = sprites.player.size()[0] / -2;
 		this.Velocity = 0.0;
 	}
 
 	draw()
 	{
-		// todo
-		ctx.fillStyle = "rgb(200, 200, 40)"
-		ctx.fillRect( this.X - 40, this.Y, 80, 50 );
+		ctx.fillStyle = "rgb(200, 200, 40)";
+		sprites.player.draw( this.X + this.xoff, this.Y );
 	}
 
 	update( deltaT )
@@ -115,19 +171,34 @@ class Player
 
 class Enemy
 {
-	constructor( x, y )
+	constructor( x, y, type )
 	{
 		this.X = x;
 		this.Y = y;
 		this.stage = 0;
 		this.stageCounter = 0.0;
+
+		if ( this.Type == 2 )
+		{
+			this.Sprite = sprites.enemyC;
+		}
+		else if ( this.Type == 1 )
+		{
+			this.Sprite = sprites.enemyB;
+		}
+		else
+		{
+			this.Sprite = sprites.enemyA;
+		}
+
+		this.off = this.Sprite.size();
+		this.off = [ this.off[0] / 2, this.off[1] / 2 ];
 	}
 
 	draw()
 	{
-		// todo
 		ctx.fillStyle = "rgb(200, 40, 200)"
-		ctx.fillRect( this.X - 40, this.Y - 50, 80, 50 );
+		this.Sprite.draw( this.X - this.off[0], this.Y - this.off[1] );
 	}
 
 	update( deltaT )
@@ -199,6 +270,47 @@ class Bullet
 		this.Y -= deltaT * bulletSpeed;
 	}
 }
+
+
+class Sprite
+{
+	constructor(...spriteData)
+	{
+		this.spriteData = spriteData;
+
+		if ( this.spriteData.length == 0 )
+			console.error( "empty sprite" );
+
+		var ln = this.spriteData[0].length;
+		for ( var i = 0; i < this.spriteData.length; i++ )
+		{
+			if ( this.spriteData[i].length != ln )
+				console.error( "inconsistent sprite size!" );
+		}
+	}
+
+	size()
+	{
+		return [ spriteSize * this.spriteData[0].length, spriteSize * this.spriteData.length ];
+	}
+
+	draw( x, y )
+	{
+		for ( var i = 0; i < this.spriteData.length; i++ )
+		{
+			for ( var j = 0; j < this.spriteData[i].length; j++ )
+			{
+				if ( this.spriteData[i].substr(j,1) == "x" )
+				{
+					ctx.fillRect( x + spriteSize*j, y + spriteSize*i, spriteSize + 0.7, spriteSize + 0.7 );
+				}
+			}
+		}
+	}
+}
+
+
+
 
 window.addEventListener( "keydown", function(event)
 {
