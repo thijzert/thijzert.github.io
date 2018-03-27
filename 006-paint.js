84b6f958f6aeb44aca1ecaec6f1a406e528e79b7
Thijs van Dijk <github@inurbanus.nl>
2018-03-27 16:44:07 +0200

    Fuzzy finding

--- 006-paint.js
+++ 006-paint.js
@@ -264,25 +264,93 @@ var RegisterCharacter = function( glyph, stroke, descriptions )
 	});
 };
 
-var LookupCharacter = function( stroke )
+var LookupCharacter = (function()
 {
-	//stroke = stroke.trim().split(/ +/);
-	var jstr = stroke.join("");
-
-	if ( ! stroke.length in characterDatabase )
-		return null;
-
-	for ( var i = 0; i < characterDatabase[stroke.length].length; i++ )
+	// Difference in starting square
+	var sqdif = function( a, b )
 	{
-		if ( characterDatabase[stroke.length][i].stroke.join("") == jstr )
-			return characterDatabase[stroke.length][i];
-	}
+		var dx = a.charCodeAt(0) - b.charCodeAt(0);
+		var dy = a.charCodeAt(1) - b.charCodeAt(1);
 
-	// TODO: fuzzy finding
+		if ( dx == 0 )  return dy;
+		if ( dy == 0 )  return dx;
 
-	return null;
-};
+		return Math.sqrt( dx*dx + dy*dy );
+	};
 
+	// Difference in direction
+	var dirdif = function( a, b )
+	{
+		if ( a == "" )
+		{
+			if ( b == "" )
+				return 0;
+			return 1;
+		}
+		else if ( b == "" )
+			return 1;
+
+		var s0 = a.charCodeAt(0) - b.charCodeAt(0);
+		if ( s0 < 0 )  s0 *= -1;
+		s0 *= 0.3;
+
+		var s1a = dirdif( a.substr(1), b.substr(1) );
+		var s1b = s1a;
+		if ( a.length > b.length )
+			s1b = dirdif( a.substr(1), b );
+		else if ( b.length < a.length )
+			s1b = dirdif( a, b.substr(1) );
+
+		if ( s1b < s1a )
+			return s0 + s1b;
+		return s0 + s1a;
+	};
+
+
+	var sqrt = new Array(20);
+	for ( var i = 0; i < 20; i++ )
+		sqrt[i] = Math.sqrt(i);
+
+	var sdif = function( a, b )
+	{
+		var d0 = sqdif( a, b );
+		var d1 = dirdif( a.substr(2), b.substr(2) );
+		d1 /= sqrt[a.length];
+
+		return d0*0.4 + d1*1.1;
+	};
+
+	return (function( stroke )
+	{
+		//stroke = stroke.trim().split(/ +/);
+		var jstr = stroke.join("");
+
+		if ( ! stroke.length in characterDatabase )
+			return null;
+
+		var wiener = null;
+		var dmax = 1000.0;
+
+		for ( var i = 0; i < characterDatabase[stroke.length].length; i++ )
+		{
+			var d = 0.0;
+			for ( var j = 0; j < stroke.length; j++ )
+			{
+				d += sdif( characterDatabase[stroke.length][i].stroke[j], stroke[j] );
+
+				if ( d > dmax )  break;
+			}
+
+			if ( d < dmax )
+			{
+				wiener = characterDatabase[stroke.length][i];
+				dmax = d;
+			}
+		}
+
+		return wiener;
+	});
+})();
 
 
 (function()
